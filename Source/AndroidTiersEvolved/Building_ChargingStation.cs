897c31029c5ae57a800775eb0a262015a1d4e1d4
RedHotKomet Home <r0893918@student.luca-arts.be>
2023-01-10 21:53:23 +0100

    added a charging station building (not working correct yet)

---
+++ Source/AndroidTiersEvolved/Building_ChargingStation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RimWorld;
+using Verse;
+
+namespace ATE
+{
+    public class Building_ChargingStation : Building
+    {
+        public override void SpawnSetup(Map map, bool respawningAfterLoad)
+        {
+            base.SpawnSetup(map, respawningAfterLoad);
+        }
+    }
+}
